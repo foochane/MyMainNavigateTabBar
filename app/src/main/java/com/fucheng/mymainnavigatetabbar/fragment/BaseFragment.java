5c8242e83c004a96364a9fad27663cf12e749d72
foochane <foochengkm@gmail.com>
2017-04-29 16:01:01 +0800

    添加底部标签导航

---
+++ app/src/main/java/com/fucheng/mymainnavigatetabbar/fragment/BaseFragment.java
@@ -0,0 +1,6 @@
+package com.fucheng.mymainnavigatetabbar.fragment;
+
+import android.app.Fragment;
+
+public class BaseFragment extends Fragment {
+}
---
+++ app/src/main/java/com/fucheng/mymainnavigatetabbar/fragment/MomentsFragment.java
@@ -0,0 +1,19 @@
+package com.fucheng.mymainnavigatetabbar.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fucheng.mymainnavigatetabbar.R;
+
+
+public class MomentsFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_moments, container, false);
+    }
+}
